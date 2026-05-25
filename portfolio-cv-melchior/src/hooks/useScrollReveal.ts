c22faa1a4c383a2f5eecbd97dac3e8ad771ba5db
MelchiorObame <40194959+MelchiorObame@users.noreply.github.com>
2026-05-25 18:33:21 +0200

    feat: move to ./

--- portfolio-cv-melchior/src/hooks/useScrollReveal.ts
+++
@@ -1,8 +0,0 @@
-import { useRef } from 'react'
-import { useInView } from 'framer-motion'
-
-export function useScrollReveal(threshold = 0.12) {
-  const ref = useRef<HTMLDivElement>(null)
-  const inView = useInView(ref, { once: true, margin: '0px 0px -50px 0px', amount: threshold })
-  return { ref, inView }
-}
